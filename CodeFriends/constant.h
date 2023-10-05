fafa93028b99c81fa878cfd84e6e48a312f3cee0
ruxia.TJY <ruxia.tjy@qq.com>
2023-10-05 12:37:41 +0800

    add constant.h file for constant string

--- CodeFriends/constant.h
+++ CodeFriends/constant.h
@@ -1,3 +1,17 @@
-#pragma once
+#ifndef CONSTANT_H
+#define CONSTANT_H
+
+#define DEBUG
 
 #define VERSION "0.0.0.1"
+
+#define AUTHOR "JiayuTu"
+#define EMAIL "ruxia.tjy@qq.com"
+
+#define AUTHOR_AND_EMAIL "JiayuTu<ruxia.tjy@qq.com>"
+
+#define DESCRIPTION "Friends help you for manage code"
+
+#define DATABASE_FORMAT ("yyyy-MM-dd HH:mm:ss")
+
+#endif // !CONSTANT_H
